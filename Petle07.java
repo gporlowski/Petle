98a8c8b53d95e83d19bba7b4cc8b8303559c5b89
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 22:09:49 +0100

    Pętle 7

--- Petle07.java
+++ Petle07.java
@@ -1,4 +1,4 @@
-public class Petle7 {
+public class Petle07 {
     public static void main(String[] args) {
         int firstWord = 5, difference = 2, numberOfWords = 459, sum = 0;
         for (int i = 1; i <= numberOfWords; i++) {
