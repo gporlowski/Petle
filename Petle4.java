5d17b41a0a33d55b8459d5a58cbe37522056c558
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 21:21:20 +0100

    Pętle 4

---
+++ Petle4.java
@@ -0,0 +1,10 @@
+public class Petle4 {
+    public static void main(String[] args) {
+        int from = 0, to = 100;
+        for (int i = from; i <= to; i++) {
+            if (i % 2 == 0) {
+                System.out.println(i);
+            }
+        }
+    }
+}
