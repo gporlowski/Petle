8cc3dc478b4699aad472b597e9e9d9b2624533ce
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 21:19:42 +0100

    Pętle 3

---
+++ Petle3.java
@@ -0,0 +1,8 @@
+public class Petle3 {
+    public static void main(String[] args) {
+        int from = 100, to = 1;
+        for (int i = from; i >= to; i--) {
+            System.out.println(i);
+        }
+    }
+}
