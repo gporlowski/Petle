b7f8883b527ec8623109bb5000741aae47723e37
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 21:17:03 +0100

    Pętle 1

---
+++ Petle1.java
@@ -0,0 +1,8 @@
+public class Petle1 {
+    public static void main(String[] args) {
+        int from = 5, to = 100;
+        for (int i = from; i <= to; i++) {
+            System.out.println(i);
+        }
+    }
+}
