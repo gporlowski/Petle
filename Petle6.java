02c9b54412666341b9511f870cc92ea3f39040d8
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 21:32:48 +0100

    Pętle 6

---
+++ Petle6.java
@@ -0,0 +1,9 @@
+public class Petle6 {
+    public static void main(String[] args) {
+        int from = 1, to = 459, sum = 0;
+        for (int i = from; i <= to; i++) {
+            sum += i;
+        }
+        System.out.println("Suma liczb z zakresu od " + from + " do " + to + " wynosi: " + sum);
+    }
+}
