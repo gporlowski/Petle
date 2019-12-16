ecb0cb0b0e1cf55d8540207d42d962478136f681
gporlowski <58628126+gporlowski@users.noreply.github.com>
2019-12-16 21:54:08 +0100

    Pętle 8

---
+++ Petle8.java
@@ -0,0 +1,11 @@
+public class Petle8 {
+    public static void main(String[] args) {
+        int x = 10;
+        for (int i = 0; i < x; i++) {
+            for (int j = 0; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
